89c55256adb5ffa34108c302b6a5a17f6c821ebf
Kevin Rojess <102902017+kevinrojess@users.noreply.github.com>
2022-11-30 14:25:57 +0400

    First commit.

---
+++ Assignment-4/UserModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+// Schema
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: false
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        dob: {
+            type: Date,
+            required: false
+        },
+        avatar: {
+            type: String,
+            required: false
+        },
+        dateCreated: {
+            type: Date,
+            required: true,
+            default: Date.now
+        },
+        subscription: {
+            type: Boolean,                //  <--- Subscription Element
+            required: false
+        }
+    }
+);
+
+
+
+// Model
+const UserModel = mongoose.model('users', UserSchema);
+
+// Export the model
+module.exports = UserModel;
